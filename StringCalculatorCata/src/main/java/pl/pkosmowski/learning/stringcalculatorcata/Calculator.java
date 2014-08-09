7ef55bec7e3c0041c3768926a7f77332ecfb0110
Piotr Kosmowski <kospiotr@gmail.com>
2014-08-09 19:25:51 +0200

    Refactoring

--- StringCalculatorCata/src/main/java/pl/pkosmowski/learning/stringcalculatorcata/Calculator.java
+++ StringCalculatorCata/src/main/java/pl/pkosmowski/learning/stringcalculatorcata/Calculator.java
@@ -1,6 +1,8 @@
 package pl.pkosmowski.learning.stringcalculatorcata;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import static java.util.Arrays.asList;
 import java.util.List;
 
 /**
@@ -10,14 +12,29 @@ import java.util.List;
 public class Calculator {
 
 	public int add(String arguments) {
-		Integer result = 0;
 
-		List<String> argumentList = new ArrayList<>();
-		for (String argument : arguments.split(",")) {
-			argumentList.add(argument);
-		}
+		List<String> argumentList = asList(arguments.split(","));
 		System.out.println("argumentList = " + argumentList);
 
+		List<Integer> numbers = extractNumbers(argumentList);
+		System.out.println("numbers = " + numbers);
+
+		int result = sumNumbers(numbers);
+		System.out.println("result = " + result);
+
+		return result;
+
+	}
+
+	private Integer sumNumbers(List<Integer> numbers) {
+		int result = 0;
+		for (Integer number : numbers) {
+			result += number;
+		}
+		return result;
+	}
+
+	private List<Integer> extractNumbers(List<String> argumentList) {
 		List<Integer> numbers = new ArrayList<>();
 		for (String numberAsString : argumentList) {
 			try {
@@ -25,15 +42,7 @@ public class Calculator {
 			} catch (NumberFormatException numberFormatException) {
 			}
 		}
-		System.out.println("numbers = " + numbers);
-
-		for (Integer number : numbers) {
-			result += number;
-		}
-		System.out.println("result = " + result);
-
-		return result;
-
+		return numbers;
 	}
 
 }
